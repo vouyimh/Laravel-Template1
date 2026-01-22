18ca8515d8ef50c2b904868c84caf0652958aa63
sautsokreach <sautsokreach@gmail.com>
2026-01-22 20:53:06 +0700

    test

--- resources/views/layouts/sections/navbar/navbar-partial.blade.php
+++ resources/views/layouts/sections/navbar/navbar-partial.blade.php
@@ -1,25 +1,27 @@
 @php
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Route;
+    use Illuminate\Support\Facades\Auth;
+    use Illuminate\Support\Facades\Route;
 @endphp
 
 <!--  Brand demo (display only for navbar-full and hide on below xl) -->
-@if(isset($navbarFull))
-<div class="navbar-brand app-brand demo d-none d-xl-flex py-0 me-4">
-    <a href="{{url('/')}}" class="app-brand-link gap-2">
-        <span class="app-brand-logo demo">@include('_partials.macros')</span>
-        <span class="app-brand-text demo menu-text fw-bold text-heading">{{config('variables.templateName')}}</span>
-    </a>
-</div>
+@if (isset($navbarFull))
+    <div class="navbar-brand app-brand demo d-none d-xl-flex py-0 me-4">
+        <a href="{{ url('/') }}" class="app-brand-link gap-2">
+            <span class="app-brand-logo demo">@include('_partials.macros')</span>
+            <span
+                class="app-brand-text demo menu-text fw-bold text-heading">{{ config('variables.templateName') }}</span>
+        </a>
+    </div>
 @endif
 
 <!-- ! Not required for layout-without-menu -->
-@if(!isset($navbarHideToggle))
-<div class="layout-menu-toggle navbar-nav align-items-xl-center me-4 me-xl-0 {{ isset($contentNavbar) ?' d-xl-none ' : '' }}">
-    <a class="nav-item nav-link px-0 me-xl-6" href="javascript:void(0)">
-        <i class="icon-base bx bx-menu icon-md"></i>
-    </a>
-</div>
+@if (!isset($navbarHideToggle))
+    <div
+        class="layout-menu-toggle navbar-nav align-items-xl-center me-4 me-xl-0 {{ isset($contentNavbar) ? ' d-xl-none ' : '' }}">
+        <a class="nav-item nav-link px-0 me-xl-6" href="javascript:void(0)">
+            <i class="icon-base bx bx-menu icon-md"></i>
+        </a>
+    </div>
 @endif
 
 <div class="navbar-nav-right d-flex align-items-center" id="navbar-collapse">
@@ -34,7 +36,7 @@ use Illuminate\Support\Facades\Route;
     <ul class="navbar-nav flex-row align-items-center ms-auto">
         <!-- Place this tag where you want the button to render. -->
         <!-- <li class="nav-item lh-1 me-4">
-            <a class="github-button" href="{{config('variables.repository')}}" data-icon="octicon-star" data-size="large" data-show-count="true" aria-label="Star themeselection/sneat-html-laravel-admin-template-free on GitHub">Star</a>
+            <a class="github-button" href="{{ config('variables.repository') }}" data-icon="octicon-star" data-size="large" data-show-count="true" aria-label="Star themeselection/sneat-html-laravel-admin-template-free on GitHub">Star</a>
         </li> -->
 
         <!-- User -->
@@ -50,11 +52,12 @@ use Illuminate\Support\Facades\Route;
                         <div class="d-flex">
                             <div class="flex-shrink-0 me-3">
                                 <div class="avatar avatar-online">
-                                    <img src="{{ asset('assets/img/avatars/1.png') }}" alt class="w-px-40 h-auto rounded-circle">
+                                    <img src="{{ asset('assets/img/avatars/1.png') }}" alt
+                                        class="w-px-40 h-auto rounded-circle">
                                 </div>
                             </div>
                             <div class="flex-grow-1">
-                                <h6 class="mb-0">John Doe</h6>
+                                {{-- <h6 class="mb-0">John Doe</h6> --}}
                                 <small class="text-muted">Admin</small>
                             </div>
                         </div>
@@ -73,10 +76,17 @@ use Illuminate\Support\Facades\Route;
                     <div class="dropdown-divider my-1"></div>
                 </li>
                 <li>
-                    <a class="dropdown-item" href="javascript:void(0);">
-                        <i class="icon-base bx bx-power-off icon-md me-3"></i><span>Log Out</span>
-                    </a>
+                    <form method="POST" action="{{ route('logout') }}" id="logout-form">
+                        @csrf
+
+                        <a class="dropdown-item" href="#"
+                            onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                            <i class="icon-base bx bx-power-off icon-md me-3"></i>
+                            <span>Log Out</span>
+                        </a>
+                    </form>
                 </li>
+
             </ul>
         </li>
         <!--/ User -->
