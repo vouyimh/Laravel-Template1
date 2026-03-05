ce28f4bce705cb33bcf3189da485450628fb48e0
HoutVouyIm <vouyimhout0962422747@gmail.com>
2026-03-05 15:28:12 +0700

    update 5

--- vite.config.js
+++ vite.config.js
@@ -35,7 +35,6 @@ const CoreScssFiles = GetFilesArray('resources/assets/vendor/scss/**/!(_)*.scss'
 const FontsScssFiles = GetFilesArray('resources/assets/vendor/fonts/!(_)*.scss');
 const FontsJsFiles = GetFilesArray('resources/assets/vendor/fonts/**/!(_)*.js');
 const FontsCssFiles = GetFilesArray('resources/assets/vendor/fonts/**/!(_)*.css');
-const FontsAwesome = GetFilesArray('public/CSS/font_awesome/*.css');
 
 export default defineConfig({
   plugins: [
@@ -52,8 +51,7 @@ export default defineConfig({
         ...LibsCssFiles,
         ...FontsScssFiles,
         ...FontsJsFiles,
-        ...FontsCssFiles,
-        ...FontsAwesome
+        ...FontsCssFiles
       ],
       refresh: true
     }),
